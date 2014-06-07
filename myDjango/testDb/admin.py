d0a0afedc32c452a463a2d794ccf5c88251305a3
root <root@d3ba1b8b-5bfd-8643-be6e-a371e5a508fa>
2014-06-07 08:08:11 +0000

    定制Nametable表的管理界面
    git-svn-id: svn://localhost/Python@5 d3ba1b8b-5bfd-8643-be6e-a371e5a508fa

--- myDjango/testDb/admin.py
+++ myDjango/testDb/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from testDb.models import Nametable
 
-# Register your models here.
+class NametableAdmin(admin.ModelAdmin):
+    # 显示列表中的字段
+    list_display = ('name', 'age', 'salary', 'phonenumber', 'email', 'password')
+    # 可搜索的字段
+    search_fields = ('name', 'phonenumber', 'age')
+    # 按age筛选
+    list_filter = ('age',)
+    # 按name从小到大排序
+    ordering = ('name',)
+    # 定义可被编辑的字段，name不可修改
+    fields = ('age', 'salary', 'phonenumber', 'email', 'password')
+
+admin.site.register(Nametable, NametableAdmin)
+#admin.site.register(Nametable)
